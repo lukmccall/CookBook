6b76fa2cd3bf85d710dd1a5198c17948eb65e6c7
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-12-06 00:01:44 +0100

    Add grid system

---
+++ client/src/ts/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-3">eclo</div>
+          <div className="col-12 col-lg-3">dsad</div>
+          <div className="col-12 col-lg-3">dasd</div>
+          <div className="col-12 col-lg-3">sad</div>
+        </div>
+      </div>
+    );
+  }
+}
