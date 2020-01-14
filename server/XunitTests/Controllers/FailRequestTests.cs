2ab32758eecbe1696ba78973b48e6e2b8efc2090
n.kwasniak <n.kwasniak@ocado.com>
2020-01-14 06:35:48 +0100

    jakis test

---
+++ server/XunitTests/Controllers/FailRequestTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+public class FailRequestsTests
+{
+    public FailRequestsTests()
+    {
+
+    }
+
+    [Fact]
+    public void ExecuteResultAsync()
+    {
+        Type myType = typeof(String);
+        var failReq = new FailRequest(myType, "message");
+
+        var result = failReq.ExecuteResultAsync(new ActionContext());
+        result.Should().NotBeNull();
+    }
+}
